d59a4accf6269de082e3854702be2c091e086cb1
unknown <thangbui.071192@gmail.com>
2021-10-21 20:43:34 +0700

    create controller

---
+++ app/Http/Controllers/HealthInsuranceCardController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HealthInsuranceCard;
+use Illuminate\Http\Request;
+
+class HealthInsuranceCardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\HealthInsuranceCard  $healthInsuranceCard
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HealthInsuranceCard $healthInsuranceCard)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\HealthInsuranceCard  $healthInsuranceCard
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HealthInsuranceCard $healthInsuranceCard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\HealthInsuranceCard  $healthInsuranceCard
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, HealthInsuranceCard $healthInsuranceCard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\HealthInsuranceCard  $healthInsuranceCard
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HealthInsuranceCard $healthInsuranceCard)
+    {
+        //
+    }
+}
